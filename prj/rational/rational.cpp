42faeef9b1f5aa4efe16aac705dc002a587141ea
bakinovsky-m <makbak@ya.ru>
2016-09-29 19:39:12 +0300

    beautify

--- prj/rational/rational.cpp
+++ prj/rational/rational.cpp
@@ -8,32 +8,32 @@ Rational::Rational(const int32_t num, const int32_t denum){
         normalize();
 }
 Rational Rational::operator+ (const Rational s){
-        return Rational ((num_ * s.denum_ + denum_ * s.num_), denum_ * s.denum_);
+    return Rational ((num_ * s.denum_ + denum_ * s.num_), denum_ * s.denum_);
 }
 Rational Rational::operator- (const Rational s){
-        return Rational ((num_ * s.denum_ - denum_ * s.num_), denum_ * s.denum_);
+    return Rational ((num_ * s.denum_ - denum_ * s.num_), denum_ * s.denum_);
 }
 Rational Rational::operator* (const Rational s){
-        return Rational (num_ * s.num_, denum_ * s.denum_);
+    return Rational (num_ * s.num_, denum_ * s.denum_);
 }
 Rational Rational::operator/ (const Rational s){
-        return Rational (num_ * s.denum_, denum_ * s.num_);
+    return Rational (num_ * s.denum_, denum_ * s.num_);
 }
 Rational& Rational::operator= (const Rational& s) = default;
 
 double Rational::toDouble(){
-            double n = num_;
-            double dn = denum_;
-            return n / dn;
-    }
+    double n = num_;
+    double dn = denum_;
+    return n / dn;
+}
 
 std::string Rational::toString(){
-        std::string str = "";
-        str = std::to_string(num_);
-        str += "/";
-        str += std::to_string(denum_);
-        return str;
-    }
+    std::string str = "";
+    str = std::to_string(num_);
+    str += "/";
+    str += std::to_string(denum_);
+    return str;
+}
 
 void Rational::normalize(){
     int nod_ = nod(num_, denum_);
