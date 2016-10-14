89db5961fc38e57de74579e314d8d8eedfb8cf11
bakinovsky-m <makbak@ya.ru>
2016-10-14 15:52:32 +0300

    vec2d: consts

--- prj/vec2d/vec2d.cpp
+++ prj/vec2d/vec2d.cpp
@@ -10,27 +10,27 @@ Vec2d::Vec2d(const double b_x, const double b_y, const double e_x, const double 
     y = e_y - b_y;
 }
 
-bool Vec2d::operator== (const Vec2d v){
+bool Vec2d::operator== (const Vec2d v) const {
     return(fabs(x - v.x) <= EPSILON && fabs(y - v.y) <= EPSILON);
 }
-bool Vec2d::operator!= (const Vec2d v){
+bool Vec2d::operator!= (const Vec2d v) const {
     return(!operator==(v));
 }
 
-Vec2d Vec2d::operator+ (const Vec2d v){
+Vec2d Vec2d::operator+ (const Vec2d v) const {
     return Vec2d(x + v.x, y + v.y);
 }
-Vec2d Vec2d::operator- (const Vec2d v){
+Vec2d Vec2d::operator- (const Vec2d v) const{
     return Vec2d(x - v.x, y - v.y);
 }
-Vec2d Vec2d::operator* (const double d){
+Vec2d Vec2d::operator* (const double d) const {
     return Vec2d(x * d, y * d);
 }
-Vec2d Vec2d::operator* (const Vec2d v){
+Vec2d Vec2d::operator* (const Vec2d v) const {
     return Vec2d(x * v.x, y * v.y);
 }
 
-double Vec2d::operator[] (const int ind){
+double Vec2d::operator[] (const int ind) const{
     if (ind == 0){
         return x;
     } else if(ind == 1){
--- prj/vec2d/vec2d.h
+++ prj/vec2d/vec2d.h
@@ -11,16 +11,16 @@ public:
     Vec2d(const double x_, const double y_);
     Vec2d(const double b_x, const double b_y, const double e_x, const double e_y);
     
-    bool operator== (const Vec2d v);
-    bool operator!= (const Vec2d v);
+    bool operator== (const Vec2d v) const;
+    bool operator!= (const Vec2d v) const;
 
-    Vec2d operator+ (const Vec2d v);
-    Vec2d operator- (const Vec2d v);
-    Vec2d operator* (const double d);
-    Vec2d operator* (const Vec2d v);
+    Vec2d operator+ (const Vec2d v) const;
+    Vec2d operator- (const Vec2d v) const;
+    Vec2d operator* (const double d) const;
+    Vec2d operator* (const Vec2d v) const;
     Vec2d& operator= (const Vec2d& v) = default;
 
-    double operator[] (const int ind);
+    double operator[] (const int ind) const;
 
     std::string toString();
     double length();
