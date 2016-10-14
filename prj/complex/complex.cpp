fee113c4d7b6f3dd7555275d38725bbe49499a4a
bakinovsky-m <makbak@ya.ru>
2016-10-14 15:22:17 +0300

    complex: consts everywhere

--- prj/complex/complex.cpp
+++ prj/complex/complex.cpp
@@ -1,35 +1,35 @@
 #include "complex.h"
 
 Complex::Complex() : Complex(0.0, 0.0){}
-Complex::Complex(double real): Complex(real, 0.0){}
-Complex::Complex(double real, double img) : re(real), im(img){}
+Complex::Complex(const double real): Complex(real, 0.0){}
+Complex::Complex(const double real, const double img) : re(real), im(img){}
 
-bool Complex::operator== (Complex c) {
+bool Complex::operator== (const Complex c) const {
     if (fabs(re - c.re) <= EPSILON && fabs(im - c.im) <= EPSILON){
         return true;
     } else {
         return false;
     }
 }
-bool Complex::operator!= (Complex c) {
+bool Complex::operator!= (const Complex c) const {
     return (!operator==(c));
 }
 
-Complex Complex::operator+ (Complex c){
+Complex Complex::operator+ (const Complex c) const {
     return Complex(re + c.re, im + c.im);
 }
-Complex Complex::operator- (Complex c){
+Complex Complex::operator- (const Complex c) const {
     return Complex(re - c.re, im - c.im);
 }
-Complex Complex::operator* (Complex c){
+Complex Complex::operator* (const Complex c) const {
     return Complex(re * c.re - im * c.im, re * c.im + im * c.re);
 }
-Complex Complex::operator/ (Complex c){
+Complex Complex::operator/ (const Complex c) const {
     double zn = c.re * c.re + c.im * c.im;
     return Complex((re * c.re + im * c.im) / zn, (im * c.re - re * c.im) / zn);
 }
 
-std::string Complex::toString(){
+std::string Complex::toString() const {
     std::string res = "";
     res = std::to_string(re);
     res += " + ";
--- prj/complex/complex.h
+++ prj/complex/complex.h
@@ -11,15 +11,15 @@ struct Complex  {
     const double EPSILON = 0.000000000001;
 
     Complex();
-    Complex(double real);
-    Complex(double real, double img);
-    bool operator==(Complex c);
-    bool operator!= (Complex c);
-    Complex operator+ (Complex c);
-    Complex operator- (Complex c);
-    Complex operator* (Complex c);
-    Complex operator/ (Complex c);
-    std::string toString();
+    Complex(const double real);
+    Complex(const double real, const double img);
+    bool operator==(const Complex c) const;
+    bool operator!= (const Complex c) const;
+    Complex operator+ (const Complex c) const;
+    Complex operator- (const Complex c) const;
+    Complex operator* (const Complex c) const;
+    Complex operator/ (const Complex c) const;
+    std::string toString() const;
     Complex& operator= (Complex& c) = default;
 };
 #endif
