b8ac3f373b1ed96d0c7d3e185cb89cf380db5acd
bakinovsky-m <makbak@ya.ru>
2016-11-15 14:05:59 +0300

    stackonlist: reworked to template

--- prj/stackonlist/stackonlist.h
+++ prj/stackonlist/stackonlist.h
@@ -1,28 +1,73 @@
 #ifndef HG_STACKONLIST_H
 #define HG_STACKONLIST_H
 
-#include <iostream>
+#include <ostream>
 #include <string>
+#include <stdexcept>
 
+template<class T>
 struct Node{
-    Node(const int v): value(v){}
-    int value = 0;
+    Node(const T v): value(v){}
+    bool operator==(const Node& t){
+        return (value == t.value && prev == t.prev);
+    }
+    T value = 0;
     Node * prev = nullptr;
 };
 
+template<class T>
 class StackOnlist{
 public:
     StackOnlist() = default;
-    StackOnlist(const StackOnlist& sol);
-    StackOnlist operator= (const StackOnlist& sol);
+    StackOnlist(const StackOnlist<T>& sol){
+        head = sol.head;
+    }
+    StackOnlist<T>& operator= (const StackOnlist<T>& sol){
+        head = sol.head;
+        return *this;
+    }
 
-    void push(Node& n);
-    void pop();
-    Node top() const;
+    void push(Node<T>& n){
+        if(existsInStack(n)){
+            return;
+        }
+        n.prev = head;
+        head = &n;
+    }
 
-    std::string toString() const;
+    void pop() {
+        head = head->prev;
+    }
+    Node<T> top() const {
+        return *head;
+    }
+
+    std::ostream& writeTo(std::ostream& ostrm) const {
+        Node<T> * a = head;
+        while (a != nullptr){
+            ostrm << a->value << " ";
+            a = a->prev;
+        }
+        return ostrm; 
+    }
 private:
-    Node * head = nullptr;
+    Node<T> * head = nullptr;
+
+    bool existsInStack(const Node<T>& n){
+        Node<T> * a = head;
+        while(a != nullptr){
+            if(*a == n){
+                return true;
+            }
+            a = a->prev;
+        }
+        return false;
+    }
 };
+
+template<class T>
+inline std::ostream& operator<< (std::ostream& ostrm, const StackOnlist<T>& sol){
+    return sol.writeTo(ostrm);
+}
 
 #endif
--- prj/stackonlist/test.cpp
+++ prj/stackonlist/test.cpp
@@ -3,13 +3,19 @@
 #include "stackonlist.h"
 
 int main(){
-    Node n1 = Node(5);
-    Node n2 = Node(6);
-    StackOnlist sol;
+    Node<int> n1 = Node<int>(5);
+    Node<int> n2 = Node<int>(6);
+    StackOnlist<int> sol;
+    StackOnlist<int> sol2 = sol;
+
     sol.push(n1);
-    std::cout << sol.toString() << std::endl;
+    std::cout << sol << std::endl;
     sol.push(n2);
-    std::cout << sol.toString() << std::endl;
+    std::cout << sol << std::endl;
+    sol.push(n2);
+    std::cout << sol << std::endl;
+    sol.pop();
+    std::cout << sol << std::endl;
 
     std::cout << "n1.prev" << std::endl;
     std::cout << n1.prev << std::endl;
